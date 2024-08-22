f3e06ea7e62c2a00a62adfbd4eb04d87293d986f
Faishal AR <Aswadalansori@gmail.com>
2024-08-23 06:19:56 +0800

    pengen pulang

---
+++ frontend/src/page/index.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import './index.css';
+
+export default function Main() {
+  return (
+    <div className='main-container'>
+      <div className='imac'>
+        <div className='rectangle' />
+        <div className='rectangle-1' />
+        <div className='group'>
+          <span className='nft-certificate'>NFT Certificate</span>
+          <div className='group-2'>
+            <span className='no-certificate'>No. Certificate</span>
+            <div className='jgfvkjhnjn' />
+            <div className='line' />
+            <span className='name'>Name</span>
+            <div className='isallkun-ridha' />
+            <div className='line-3' />
+          </div>
+          <button className='component'>
+            <span className='generate'>Generate</span>
+            <div className='rectangle-4' />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
